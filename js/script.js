032f0d491a2ab88748bf6a573806cbca04b57b58
Evgeniy <zver@gmail.com>
2022-10-18 12:13:46 +0700

    add axios

--- js/script.js
+++ js/script.js
@@ -190,9 +190,9 @@ document.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    getResource('http://localhost:3000/menu')
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
         });
